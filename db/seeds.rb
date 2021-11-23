01d924f2c6db1105a0d9f39a3feac81c57c97d0e
Fabricio Camargo <fabriciombc@gmail.com>
2021-11-23 07:41:52 -0800

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Item.destroy_all
+
+User.create!(nickname: 'Leon', email: 'leon@test.com', password: '123123')
+User.create!(nickname: 'Mice', email: 'mice@test.com', password: '123123')
+User.create!(nickname: 'Mamma Bear', email: 'bear@test.com', password: '123123')
+
+Item.create!(name: 'Chain Saw', price: 500, description: 'okok', user_id: User.first)
+Item.create!(name: 'Axe', price: 500, description: 'okok', user_id: User.first)
+Item.create!(name: 'Sword', price: 500, description: 'okok', user_id: User.last)
