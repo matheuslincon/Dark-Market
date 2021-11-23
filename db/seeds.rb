9bf7066e9e17204a785dc09e0f874bea83367880
Fabricio Camargo <fabriciombc@gmail.com>
2021-11-23 09:11:27 -0800

    added puts msg to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaned users and items"
 
+User.destroy_all
 Item.destroy_all
+
+puts "created users"
 
 User.create!(nickname: 'Leon', email: 'leon@test.com', password: '123123')
 User.create!(nickname: 'Mice', email: 'mice@test.com', password: '123123')
 User.create!(nickname: 'Mamma Bear', email: 'bear@test.com', password: '123123')
 
-Item.create!(name: 'Chain Saw', price: 500, description: 'okok', user_id: User.first)
-Item.create!(name: 'Axe', price: 600, description: 'okok', user_id: User.first)
-Item.create!(name: 'Sword', price: 1500, description: 'okok', user_id: User.last)
+puts "created items"
+
+Item.create!(name: 'Chain Saw', price: 500, description: 'okok', user: User.first)
+Item.create!(name: 'Axe', price: 600, description: 'okok', user: User.first)
+Item.create!(name: 'Sword', price: 1500, description: 'okok', user: User.last)
+
+puts "done"
