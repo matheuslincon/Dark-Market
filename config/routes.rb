ef34636f495c7389a1aa9c0b48c12f71fd6ef2c3
talitalayane <talitalayane@hotmail.com>
2021-11-24 13:52:41 -0300

    confitos ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-<<<<<<< HEAD
-    get '/my_items', to: 'pages#my_items', as: 'my_items'
-=======
   get '/my_items', to: 'pages#my_items', as: 'my_items'
->>>>>>> master
   devise_for :users
   resources :items do
     resources :orders, only: %i[create]
