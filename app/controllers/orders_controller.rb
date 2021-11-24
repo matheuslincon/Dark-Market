526a486e5a3f385b3bfe1ba328ac36138da3e371
Matheus Andrade <matheuslincon50@gmail.com>
2021-11-24 15:14:48 -0300

    sold item logic done

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,6 +4,8 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = Order.new(item: @item, user: current_user)
     if @order.save
+      @item.available = false
+      @item.save
       redirect_to my_items_path, notice: "Your item has been purchased"
     else
       redirect_to @item, notice: "Failed"
