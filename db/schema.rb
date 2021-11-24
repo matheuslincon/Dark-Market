5f33910639d6618437102415966955456939e9c4
Matheus Andrade <matheuslincon50@gmail.com>
2021-11-24 18:14:28 -0300

    cloudnary

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_190539) do
-# ActiveRecord::Schema.define(version: 2021_11_24_173818) do
+ActiveRecord::Schema.define(version: 2021_11_24_192608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +51,9 @@ ActiveRecord::Schema.define(version: 2021_11_24_190539) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "available", default: true
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
