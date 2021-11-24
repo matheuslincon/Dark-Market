f2115611d5599915e5bb78ca5757acce67f1694b
talitalayane <talitalayane@hotmail.com>
2021-11-24 12:45:16 -0300

    create order done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   def home
   end
 
-  def my_items; end
+  def my_items
+  @my_items = current_user.items
+  @bought_items = current_user.orders
+  # @sold_items = @my_items.where.not(order: false)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+    get '/my_items', to: 'pages#my_items', as: 'my_items'
   devise_for :users
-  resources :items
+  resources :items do
+    resources :orders, only: %i[create]
+  end
 end
