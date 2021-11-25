c6d4072ff55c907a92bb81e02c34bca4b047451c
Matheus Andrade <matheuslincon50@gmail.com>
2021-11-25 15:31:28 -0300

    added categories to items

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'devise'
 gem 'cloudinary'
+gem 'pg_search'
 
 group :development, :test do
   gem 'pry-byebug'
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,11 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   has_one_attached :photo
+
+  include PgSearch::Model
+  pg_search_scope :search,
+    against: [ :name, :categories ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
