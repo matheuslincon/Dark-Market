c4e3576f98f213349efb3c9a515fe9c31aa39579
Matheus Andrade <matheuslincon50@gmail.com>
2021-11-25 19:04:51 -0300

    updated version

--- db/schema.rb
+++ db/schema.rb
@@ -51,6 +51,9 @@ ActiveRecord::Schema.define(version: 2021_11_25_191731) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "available", default: true
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.string "categories"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
